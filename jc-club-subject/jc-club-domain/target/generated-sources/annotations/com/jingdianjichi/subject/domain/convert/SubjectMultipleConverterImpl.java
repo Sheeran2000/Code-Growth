b56025158c48ab60fe05d53016ddb952e8207cd7
王行访 <2397522934@qq.com>
2024-10-10 18:00:15 +0800

    1.3 题目接口开发 - 工厂＋策略的模式 实现查询题目列表和详情(多选 单项 简单 判断)

---
+++ jc-club-subject/jc-club-domain/target/generated-sources/annotations/com/jingdianjichi/subject/domain/convert/SubjectMultipleConverterImpl.java
@@ -0,0 +1,30 @@
+package com.jingdianjichi.subject.domain.convert;
+
+import com.jingdianjichi.subject.domain.entity.SubjectAnswerBO;
+import com.jingdianjichi.subject.infra.basic.entity.SubjectMultiple;
+import javax.annotation.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-10-07T17:17:20+0800",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 1.8.0_271 (Oracle Corporation)"
+)
+public class SubjectMultipleConverterImpl implements SubjectMultipleConverter {
+
+    @Override
+    public SubjectMultiple convertBoToEntity(SubjectAnswerBO subjectAnswerBO) {
+        if ( subjectAnswerBO == null ) {
+            return null;
+        }
+
+        SubjectMultiple subjectMultiple = new SubjectMultiple();
+
+        if ( subjectAnswerBO.getOptionType() != null ) {
+            subjectMultiple.setOptionType( subjectAnswerBO.getOptionType().longValue() );
+        }
+        subjectMultiple.setOptionContent( subjectAnswerBO.getOptionContent() );
+        subjectMultiple.setIsCorrect( subjectAnswerBO.getIsCorrect() );
+
+        return subjectMultiple;
+    }
+}
---
+++ jc-club-subject/jc-club-domain/target/generated-sources/annotations/com/jingdianjichi/subject/domain/convert/SubjectRadioConverterImpl.java
@@ -0,0 +1,28 @@
+package com.jingdianjichi.subject.domain.convert;
+
+import com.jingdianjichi.subject.domain.entity.SubjectAnswerBO;
+import com.jingdianjichi.subject.infra.basic.entity.SubjectRadio;
+import javax.annotation.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-10-07T17:17:20+0800",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 1.8.0_271 (Oracle Corporation)"
+)
+public class SubjectRadioConverterImpl implements SubjectRadioConverter {
+
+    @Override
+    public SubjectRadio convertBoToEntity(SubjectAnswerBO subjectAnswerBO) {
+        if ( subjectAnswerBO == null ) {
+            return null;
+        }
+
+        SubjectRadio subjectRadio = new SubjectRadio();
+
+        subjectRadio.setOptionType( subjectAnswerBO.getOptionType() );
+        subjectRadio.setOptionContent( subjectAnswerBO.getOptionContent() );
+        subjectRadio.setIsCorrect( subjectAnswerBO.getIsCorrect() );
+
+        return subjectRadio;
+    }
+}
